8355f132cf82fe0a3bc3452509838023f0beb65a
edwinmongare <edwinmongare15@gmail.com>
2026-06-05 16:21:08 +0300

    layout

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -1,7 +1,7 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 import config from "@payload-config";
 import "@payloadcms/next/css";
-import { RootLayout } from "@payloadcms/next/layouts";
+import { handleServerFunctions, RootLayout } from "@payloadcms/next/layouts";
 import React from "react";
 
 import { importMap } from "./admin/importMap.js";
@@ -11,8 +11,17 @@ type Args = {
   children: React.ReactNode;
 };
 
+const serverFunction = async function (args: Parameters<typeof handleServerFunctions>[0]) {
+  "use server";
+  return handleServerFunctions({
+    ...args,
+    config,
+    importMap,
+  });
+};
+
 const Layout = ({ children }: Args) => (
-  <RootLayout config={config} importMap={importMap}>
+  <RootLayout config={config} importMap={importMap} serverFunction={serverFunction}>
     {children}
   </RootLayout>
 );
