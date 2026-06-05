f9d2e68ffee26781473b6922647fffcd75428250
edwinmongare <edwinmongare15@gmail.com>
2026-06-05 16:23:16 +0300

    layout

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -2,6 +2,7 @@
 import config from "@payload-config";
 import "@payloadcms/next/css";
 import { handleServerFunctions, RootLayout } from "@payloadcms/next/layouts";
+import type { ServerFunctionClient } from "payload";
 import React from "react";
 
 import { importMap } from "./admin/importMap.js";
@@ -11,7 +12,7 @@ type Args = {
   children: React.ReactNode;
 };
 
-const serverFunction = async function (args: Parameters<typeof handleServerFunctions>[0]) {
+const serverFunction: ServerFunctionClient = async function (args) {
   "use server";
   return handleServerFunctions({
     ...args,
