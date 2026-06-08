596cbc2a1a6cbb4940d66d95573bf2a5d32c4fd2
edwinmongare <edwinmongare15@gmail.com>
2026-06-08 10:54:23 +0300

    login

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -1,20 +1,38 @@
 import { AuthCredentialsValidator } from "@/lib/validators/account-credentials-validator";
 import { publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
-import { login } from "@payloadcms/next/auth";
+import { getPayload } from "payload";
 import config from "@payload-config";
 
 export const authRouter = router({
   signIn: publicProcedure
     .input(AuthCredentialsValidator)
-    .mutation(async ({ input }) => {
+    .mutation(async ({ input, ctx }) => {
       try {
-        await login({
+        const payload = await getPayload({ config });
+        const result = await payload.login({
           collection: "users",
-          config,
-          email: input.email,
-          password: input.password,
+          data: { email: input.email, password: input.password },
         });
+
+        if (result.token) {
+          const expires = result.exp
+            ? new Date(result.exp * 1000).toUTCString()
+            : undefined;
+          ctx.resHeaders.append(
+            "Set-Cookie",
+            [
+              `payload-token=${result.token}`,
+              "Path=/",
+              "HttpOnly",
+              "SameSite=Lax",
+              expires ? `Expires=${expires}` : "",
+            ]
+              .filter(Boolean)
+              .join("; "),
+          );
+        }
+
         return { success: true };
       } catch {
         throw new TRPCError({ code: "UNAUTHORIZED" });
